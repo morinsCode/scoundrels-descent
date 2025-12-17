b0cb2a93922a2f2cdcb5a02d9ce597e019b183e8
gapsofmind <morin.ibrahim@gmail.com>
2025-12-17 11:52:55 +0100

    TDD: test for score calculation

--- ReadMeBDDTests.md
+++ ReadMeBDDTests.md
@@ -1,11 +1,13 @@
-# Angående BDD testning
+# Angående TDD
 
 - Jag har två komponenter för kravet om att skapa tester före kodning
-- Använder BDD
 - Testningen gäller komponenterna
   - src/components/FaliedScreen.tsx
-  - srx/components/CompletedRunScreen.tsx
-- Komopnenterna och props för dom är skapade innan testerna började skrivas, men är enklare placholders utan mycket funktionalitet.
-- Testfilerna för dessa komponenter hittas under frontend/cypress/features och frontend/cypress/support
-- Commits för att lättare hitta:
-  -
+  - src/components/CompletedRunScreen.tsx
+- Komopnenterna och props för dom är skapade innan testerna började skrivas, men är enklare placholders.
+
+- Testfilerna för dessa komponenter hittas under frontend/cypress/component
+- COMMITS
+  - Commits startar med TDD:
+  - 67c3c88 TDD: initial cypress test file FailedScreen.cy.ts
+    - Först commit för TDD
--- frontend/cypress/component/FailedScreen.cy.tsx
+++ frontend/cypress/component/FailedScreen.cy.tsx
@@ -11,19 +11,39 @@ import { TestWrapper } from "../support/GameScreenStylingWrapper";
 
 // # Scenario: Displaying the Failed Run Screen
 describe("FailedRunScreen Component", () => {
-  // ! Testing placeholder component rendering
-  it("should render the FailedScreen component", () => {
+  // # Scenario: Display score with calculation breakdown
+  it("should display total score with monster points and health penalty breakdown", () => {
     const gameState = new GameState([]);
     gameState.stateOfRun = "failed";
     gameState.roomIndex = 5;
     gameState.player.currentHealth = 0;
+
+    gameState.player.monstersDefeated = [3, 5, 7, 14]; // Total 29 points
+    gameState.player.totalHealthLost = 10; // Health penalty 10 points
+
+    // Mock the scoreRun
+    cy.stub(gameState, "scoreRun").returns(29 - 10); // Total score 19 points
 
     cy.mount(
       <TestWrapper>
         <FailedRunScreen gameState={gameState} />
       </TestWrapper>
     );
-
-    cy.contains("Run Failed").should("be.visible");
+    // Died in Room 5
+    cy.contains("YOU DIED").should("be.visible");
+    cy.contains("5").should("be.visible");
+    // Monsters defeated
+    cy.contains(/Monster points|Score for killed monsters/i).should(
+      "be.visible"
+    );
+    cy.contains("3,5,7,14").should("be.visible");
+    //Health lost
+    cy.contains(/Health penalty|damage taken/i).should("be.visible");
+    cy.contains("10").should("be.visible");
+    // Total score
+    cy.contains(/Score:|Total Score:/i).should("be.visible");
+    cy.contains("29").should("be.visible");
+    // Final score
+    cy.contains("19").should("be.visible");
   });
 });
